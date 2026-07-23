dd5cec9f9555e64d8f8e7d87b0d651d5114d2aee
Malik Abdul Salam <75979219+MalikAbdulSalam@users.noreply.github.com>
2026-07-23 15:06:49 +0500

    data types

---
+++ 9-datatypess.py
@@ -0,0 +1,46 @@
+a =1
+# this is integer
+
+b = "digiboost"
+d = "102"
+# this is string
+
+c= 3.14
+# this is float
+
+bol = True
+# this is boolean
+
+
+# check type pf bol vraiable
+type_of_variable = type(bol)
+print(type_of_variable)
+
+
+
+
+# ###########################type conversion
+
+
+# convert d into integer
+d = int(d)
+print(type(d))
+
+
+# convrt value of c into intger
+c = int(c)
+print(type(c))
+print(c)
+
+
+# convert varibal a into float
+a = float(a)
+print(type(a))
+print(a)
+
+
+
+# convert varible c into string
+c = str(c)
+print(type(c))
+print("value of updated c is     " , c)
