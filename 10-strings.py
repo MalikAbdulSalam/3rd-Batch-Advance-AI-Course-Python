66a1b049944bb434e5815fd41de633d57bfc0bea
Malik Abdul Salam <75979219+MalikAbdulSalam@users.noreply.github.com>
2026-07-27 15:16:48 +0500

    Add files via upload

---
+++ 10-strings.py
@@ -0,0 +1,13 @@
+var_1 = "123456666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666"
+
+#for variable_name in var_1:
+#    print(do some thing here)
+
+
+for i in var_1:
+    i = int(i)
+    i = i+10
+    print(i)
+
+print("for loop ended here")
+
