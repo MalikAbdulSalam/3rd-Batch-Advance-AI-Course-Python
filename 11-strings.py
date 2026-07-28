d0b4c2f8807253ed487ad88def2fdc590f787326
Malik Abdul Salam <75979219+MalikAbdulSalam@users.noreply.github.com>
2026-07-28 14:43:11 +0500

    strings operations

---
+++ 11-strings.py
@@ -0,0 +1,40 @@
+a =  "digiboostdddddddddddddddddddddddd"
+print(a)
+
+# indexing in python
+# lef to right
+# 0.1.2.3.4.5.6....
+
+# right ot left
+# -1.-2.-3 ....
+
+
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+
+
+a =  "45123647"
+# for loop in python
+# syntax
+# for variable_nam in string:
+#     do some things
+
+for i in a:
+    i = int(i)
+    result = i +10
+    print(result)
+
+# how to check length of string
+a =  "45123647"
+type(a)
+length_of_a = len(a)
+
+print(length_of_a)
+
+# check item in string
+txt = "The best things in life are free!"
+check = "free" in txt
+print(type(check))
+print("stutus of given wors is  ",check)
